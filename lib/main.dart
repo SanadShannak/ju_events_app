753f2ab0935a4980a9ab6980fcd53d35e88b6c13
Sanad Shannak <sanadshannak.ss@gmail.com>
2024-12-06 14:03:40 +0300

    removed errors

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:temp_project/screens/authentication/login_page.dart';
 import 'package:temp_project/screens/authentication/signup_page.dart';
 import 'package:temp_project/screens/authentication/forgot_password_page.dart';
-import 'package:temp_project/screens/homePage/home_page.dart';
+
 import 'package:temp_project/utilities/theme/theme.dart';
 
 void main() => runApp(const JUEvents());
@@ -16,7 +16,7 @@ class JUEvents extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       themeMode: ThemeMode.light,
-      initialRoute: '/', 
+      initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
         '/signup': (context) => SignUpPage(),
